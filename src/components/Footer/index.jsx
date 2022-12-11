b4bcbac30aef3724c88a59959ce05730c0f1bf18
NBenvino <nicolas.benvino@gmail.com>
2022-12-11 16:41:25 -0300

    added margin

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,11 @@
 export function Footer() {
   return (
-    <div className="bottom-0 position-fixed mt-5 bg-secondary w-100 d-flex justify-content-around p-2">
-      <small>Codo a Codo - FrontEnd React 2022</small>
-      <small>Benvino Nicolás Matías</small>
-    </div>
+    <>
+      <div className="m-5"></div>
+      <div className="bottom-0 position-fixed bg-secondary w-100 d-flex justify-content-around p-1">
+        <small>Codo a Codo - FrontEnd React 2022</small>
+        <small>Benvino Nicolás Matías</small>
+      </div>
+    </>
   );
 }
