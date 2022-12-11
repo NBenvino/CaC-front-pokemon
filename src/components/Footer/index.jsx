10f297065544b80035802aa763850f6f79dc40f0
NBenvino <nicolas.benvino@gmail.com>
2022-12-11 18:01:08 -0300

    change margin

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <>
-      <div className="m-5"></div>
+      <div className="m-4"></div>
       <div className="bottom-0 position-fixed bg-secondary w-100 d-flex justify-content-around p-1">
         <small>Codo a Codo - FrontEnd React 2022</small>
         <small>Benvino Nicolás Matías</small>
